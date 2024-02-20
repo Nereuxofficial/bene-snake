16d60ad4ab2a18e2ee7a12e6113273b4235fe93c
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-02-20 19:02:09 +0100

    Favor moving in the direction of food

--- src/engine.rs
+++ src/engine.rs
@@ -1,12 +1,12 @@
 use battlesnake_game_types::compact_representation::dimensions::Dimensions;
 use battlesnake_game_types::compact_representation::standard::CellBoard;
-use battlesnake_game_types::compact_representation::{
-    CellIndex, CellNum, StandardCellBoard4Snakes11x11,
-};
+use battlesnake_game_types::compact_representation::{CellNum, StandardCellBoard4Snakes11x11};
 use battlesnake_game_types::types::{
-    HeadGettableGame, HealthGettableGame, Move, NeighborDeterminableGame, ReasonableMovesGame,
-    SizeDeterminableGame, SnakeId, VictorDeterminableGame, YouDeterminableGame,
+    FoodGettableGame, HeadGettableGame, HealthGettableGame, LengthGettableGame, Move,
+    ReasonableMovesGame, SimulableGame, SimulatorInstruments, SizeDeterminableGame,
+    SnakeIDGettableGame, SnakeId, VictorDeterminableGame, YouDeterminableGame,
 };
+use std::time::Duration;
 use tracing::info;
 
 pub fn calc_move<T: CellNum, D: Dimensions, const BOARD_SIZE: usize, const MAX_SNAKES: usize>(
@@ -14,11 +14,91 @@ pub fn calc_move<T: CellNum, D: Dimensions, const BOARD_SIZE: usize, const MAX_S
 ) -> Move {
     let mut reasonable_moves = cellboard.reasonable_moves_for_each_snake();
     let you = cellboard.you_id();
+    search_paranoid_minimax(cellboard, 3, &you)
+}
 
-    reasonable_moves
-        .find(|&(snakeid, _)| snakeid == *you)
-        .map(|(_, m)| *m.first().unwrap_or(&Move::Down))
+/// Given a cellboard, a depth and a SnakeId, this function will search for the best move
+/// using a paranoid minimax algorithm.
+/// **Inner Workings**
+/// We collect all reasonable moves in our current state and simulate each one for our current position.
+/// Then we simulate the next opponent moves and pick the least favorable outcome for us.
+/// We then simulate the next opponent moves and pick the least favorable outcome for us.
+/// Repeat until there are no more opponents left on the current board.
+pub fn search_paranoid_minimax<
+    T: CellNum,
+    D: Dimensions,
+    const BOARD_SIZE: usize,
+    const MAX_SNAKES: usize,
+>(
+    cellboard: CellBoard<T, D, BOARD_SIZE, MAX_SNAKES>,
+    depth: usize,
+    you: &SnakeId,
+) -> Move {
+    let mut reasonable_moves = cellboard.reasonable_moves_for_each_snake();
+    let mut best_move = Move::Down;
+    let simulated_boards = cellboard.simulate_with_moves(&Simulator {}, reasonable_moves);
+    simulated_boards
+        .map(|(action, a)| (evaluate_board(a, you), (action, a)))
+        .max_by(|(e, _), (other_e, _)| e.total_cmp(other_e))
+        .map(|(_, (action, _))| action.own_move())
         .unwrap_or(Move::Down)
+}
+
+fn evaluate_board<T: CellNum, D: Dimensions, const BOARD_SIZE: usize, const MAX_SNAKES: usize>(
+    cellboard: CellBoard<T, D, BOARD_SIZE, MAX_SNAKES>,
+    you: &SnakeId,
+) -> f32 {
+    evaluate_for_player(cellboard, you)
+        - cellboard
+            .get_snake_ids()
+            .iter()
+            .filter(|&id| id != you)
+            .map(|id| evaluate_for_player(cellboard, id))
+            .sum::<f32>()
+            / 3.0
+}
+
+fn evaluate_for_player<
+    T: CellNum,
+    D: Dimensions,
+    const BOARD_SIZE: usize,
+    const MAX_SNAKES: usize,
+>(
+    cellboard: CellBoard<T, D, BOARD_SIZE, MAX_SNAKES>,
+    you: &SnakeId,
+) -> f32 {
+    if cellboard.is_alive(you) {
+        let other_ids = cellboard.get_snake_ids();
+        let food = cellboard.get_all_food_as_positions();
+        let head = cellboard.get_head_as_position(you);
+        // Favor positions closer to food
+        let food_distance_avg = food
+            .iter()
+            .map(|&f| (head.manhattan_length() as i32 - f.manhattan_length() as i32).unsigned_abs())
+            .sum::<u32>()
+            / food.len() as u32;
+        food_distance_avg as f32
+            + cellboard.get_health(you) as f32 / 100.0
+            + cellboard.get_length(you) as f32 / 10.0
+            - other_ids
+                .iter()
+                .filter(|&id| id != you)
+                .map(|id| {
+                    cellboard.get_health(id) as f32 / 100.0 + cellboard.get_length(id) as f32 / 10.0
+                })
+                .sum::<f32>()
+    } else {
+        f32::MIN
+    }
+}
+
+#[derive(Debug)]
+struct Simulator {}
+
+impl SimulatorInstruments for Simulator {
+    fn observe_simulation(&self, d: Duration) {
+        info!("Simulation took {:?}", d);
+    }
 }
 
 pub fn is_lost<T: CellNum, D: Dimensions, const BOARD_SIZE: usize, const MAX_SNAKES: usize>(
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
 mod engine;
 
-use axum::body::Body;
 use axum::extract::State;
-use axum::handler::Handler;
 use axum::response::Response;
 use axum::routing::{get, post};
 use axum::{debug_handler, Json, Router};
-use battlesnake_game_types::compact_representation::dimensions::Square;
 use battlesnake_game_types::compact_representation::standard::CellBoard4Snakes11x11;
-use battlesnake_game_types::compact_representation::wrapped::CellBoard;
 use battlesnake_game_types::types::{
-    build_snake_id_map, Move, SnakeIDGettableGame, SnakeIDMap, VictorDeterminableGame,
+    build_snake_id_map, SnakeIDGettableGame, SnakeIDMap, VictorDeterminableGame,
 };
 use battlesnake_game_types::wire_representation::Game;
 use serde_json::{json, Value};
 use std::collections::HashMap;
-use std::error::Error;
 use std::sync::{Arc, Mutex};
 use tracing::info;
 
@@ -25,6 +20,8 @@ fn decode_state(
     mut text: String,
     game_states: GameStates,
 ) -> color_eyre::Result<CellBoard4Snakes11x11> {
+    #[cfg(debug_assertions)]
+    let start = std::time::Instant::now();
     #[cfg(debug_assertions)]
     info!("JSON: {}", text);
     let decoded: Game = unsafe { simd_json::serde::from_str(&mut text) }?;
@@ -37,29 +34,42 @@ fn decode_state(
                 .expect("No such game id found"),
         )
         .unwrap();
+    #[cfg(debug_assertions)]
+    info!("Deciding took: {:?}", start.elapsed());
     Ok(cellboard)
 }
 
 #[debug_handler]
 async fn get_move(State(game_states): State<GameStates>, body: String) -> Json<Value> {
     let cellboard = decode_state(body, game_states).unwrap();
-    Json(json!({"move": engine::calc_move(cellboard).to_string()}))
+    let chosen_move = engine::calc_move(cellboard).to_string();
+    Json(json!({"move": chosen_move}))
 }
 
 async fn info() -> Json<Value> {
     Json(json!({
         "apiversion": "1",
         "author": "Nereuxofficial",
-        "color": "#888888", // TODO: Choose color
-        "head": "default", // TODO: Choose head
-        "tail": "default", // TODO: Choose tail
+        "color": "#FF5E5B", // TODO: Choose color
+        "head": "mlh-gene", // TODO: Choose head
+        "tail": "mlh-gene", // TODO: Choose tail
     }))
 }
 
-// TODO: Delete the game state from the game_states map
-async fn end(body: String) -> Response {
+async fn end(State(game_states): State<GameStates>, body: String) -> Response {
     let game_state: Game = serde_json::from_str(&body).unwrap();
-    info!("Winner is {:?}", game_state.get_winner());
+    let state = game_states
+        .lock()
+        .unwrap()
+        .get(&game_state.game.id)
+        .unwrap();
+    if game_state.you_are_winner() {
+        info!("We won the game");
+    } else {
+        info!("We lost the game");
+    }
+
+    game_states.lock().unwrap().remove(&game_state.game.id);
     Response::default()
 }
 
@@ -69,7 +79,7 @@ async fn start(State(game_states): State<GameStates>, body: String) -> Response 
         "Game started with {} snakes",
         game_state.get_snake_ids().len()
     );
-    let mut snake_id_map = build_snake_id_map(&game_state);
+    let snake_id_map = build_snake_id_map(&game_state);
     game_states
         .lock()
         .unwrap()
@@ -114,6 +124,10 @@ mod tests {
     #[test]
     fn test_decode_state() {
         let game = r##"{"game":{"id":"203cd476-bd6f-4c20-8021-b222043f16e5","ruleset":{"name":"standard","version":"v1.2.3","settings":{"foodSpawnChance":15,"minimumFood":1,"hazardDamagePerTurn":0,"hazardMap":"","hazardMapAuthor":"","royale":{"shrinkEveryNTurns":0},"squad":{"allowBodyCollisions":false,"sharedElimination":false,"sharedHealth":false,"sharedLength":false}}},"map":"standard","timeout":500,"source":"custom"},"turn":0,"board":{"height":11,"width":11,"snakes":[{"id":"gs_PcT4fXmg3KPK4wgGgph6YYG6","name":"Hungry Bot","latency":"","health":100,"body":[{"x":9,"y":5},{"x":9,"y":5},{"x":9,"y":5}],"head":{"x":9,"y":5},"length":3,"shout":"","squad":"","customizations":{"color":"00cc00","head":"alligator","tail":"alligator"}},{"id":"gs_kQTcFtrXvdhXBdSy6TCKqck3","name":"Loopy Bot","latency":"","health":100,"body":[{"x":5,"y":9},{"x":5,"y":9},{"x":5,"y":9}],"head":{"x":5,"y":9},"length":3,"shout":"","squad":"","customizations":{"color":"#800080","head":"caffeine","tail":"iguana"}},{"id":"gs_vKjjkVGxJpQchxG6tYMwQHCV","name":"bene-snake-dev","latency":"","health":100,"body":[{"x":5,"y":1},{"x":5,"y":1},{"x":5,"y":1}],"head":{"x":5,"y":1},"length":3,"shout":"","squad":"","customizations":{"color":"#888888","head":"default","tail":"default"}},{"id":"gs_RFCHRmwRBWrhfQSX7x83JPcW","name":"Loopy Bot","latency":"","health":100,"body":[{"x":1,"y":5},{"x":1,"y":5},{"x":1,"y":5}],"head":{"x":1,"y":5},"length":3,"shout":"","squad":"","customizations":{"color":"#800080","head":"caffeine","tail":"iguana"}}],"food":[{"x":10,"y":4},{"x":6,"y":10},{"x":6,"y":0},{"x":0,"y":6},{"x":5,"y":5}],"hazards":[]},"you":{"id":"gs_vKjjkVGxJpQchxG6tYMwQHCV","name":"bene-snake-dev","latency":"","health":100,"body":[{"x":5,"y":1},{"x":5,"y":1},{"x":5,"y":1}],"head":{"x":5,"y":1},"length":3,"shout":"","squad":"","customizations":{"color":"#888888","head":"default","tail":"default"}}}"##;
-        let game = decode_state(game.to_string()).unwrap();
+        let game = serde_json::from_str::<Game>(game).unwrap();
+        let mut hm = HashMap::new();
+        let id_map = build_snake_id_map(&game);
+        hm.insert("203cd476-bd6f-4c20-8021-b222043f16e5".to_string(), id_map);
+        let game = decode_state(game.to_string(), Arc::new(Mutex::new(hm))).unwrap();
     }
 }
